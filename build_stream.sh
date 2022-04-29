78006ab97a929c4fa4971bd648d358d1237ea941
arstgr <78465331+arstgr@users.noreply.github.com>
2022-04-29 21:10:33 +0000

    scripts for running by pssh were added

--- build_stream.sh
+++ build_stream.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-VM_SKU=$(curl --connect-timeout 10 -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2018-04-02" | jq '.compute.vmSize')
-VM_SKU="${VM_SKU%\"}"
-VM_SKU="${VM_SKU#\"}"
-
 if test -f "stream.c"; then
     rm stream.c
 fi
---
+++ stream_pssh_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SKU type
+SKU=$1
+
+export wdir=$(pwd)
+sudo yum install pssh -y
+echo "beginning date: $(date)"
+echo $(hostname) > hosts.txt
+
+if command -v pbsnodes --version &> /dev/null
+then
+    pbsnodes -avS | grep free | awk -F ' ' '{print $1}' >> hosts.txt
+fi
+
+pssh -p 194 -t 0 -i -h hosts.txt "cd $wdir && ./stream_run_script.sh $wdir $SKU" >> stream_pssh.log 2>&1
+sleep 60
