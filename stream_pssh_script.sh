8884db199ff987bdebbb1f4b0f3d2cd52aa0a88b
ar <78465331+arstgr@users.noreply.github.com>
2022-05-20 15:35:24 -0400

    Update stream_pssh_script.sh

--- stream_pssh_script.sh
+++ stream_pssh_script.sh
@@ -15,3 +15,9 @@ fi
 
 pssh -p 32 -t 0 -i -h hosts.txt "cd $wdir && ./stream_run_script.sh $wdir $SKU" >> stream_pssh.log 2>&1
 sleep 60
+
+IFS=$'\n' read -d '' -r -a names < ./hosts.txt
+for i in ${names[@]}; do
+    echo "system: $i stream: $(grep 'Triad:' stream-$i/stream-*.log | awk '{print $2}') MB/s" >> stream-test-results.log
+done
+    
