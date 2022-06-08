4dcd56f605170ae9bf4d940d1db99faf2ddf8856
ar <78465331+arstgr@users.noreply.github.com>
2022-06-08 14:33:49 -0400

    Update stream_pssh_script.sh

--- stream_pssh_script.sh
+++ stream_pssh_script.sh
@@ -6,11 +6,11 @@ SKU=$1
 export wdir=$(pwd)
 sudo yum install pssh -y
 echo "beginning date: $(date)"
-echo $(hostname) > hosts.txt
+echo $(hostname | tr "[:upper:]" "[:lower:]") > hosts.txt
 
 if command -v pbsnodes --version &> /dev/null
 then
-    pbsnodes -avS | grep free | awk -F ' ' '{print $1}' >> hosts.txt
+    pbsnodes -avS | grep free | awk -F ' ' '{print tolower($1)}' >> hosts.txt
 fi
 
 pssh -p 32 -t 0 -i -h hosts.txt "cd $wdir && ./stream_run_script.sh $wdir $SKU" >> stream_pssh.log 2>&1
